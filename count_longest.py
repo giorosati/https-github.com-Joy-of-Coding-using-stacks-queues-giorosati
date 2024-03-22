248d06332ec2a2ed99f4881320ece84780c1f424
Giovanni <giovanni.rosati@gmail.com>
2024-03-21 22:01:05 -0500

    passed all tests

--- count_longest.py
+++ count_longest.py
@@ -2,7 +2,7 @@
 # TO PROBLEM 4: COUNT THE LONGEST SUBSEQUENCE
 #
 # NAME:         Giovanni Rosati
-# DATE:         2024-03-14
+# DATE:         2024-03-21
 # ASSIGNMENT:   Project 2: Stacks & Queues
 
 from Queue import Queue
@@ -11,13 +11,32 @@ from Queue import Queue
 # can destroy the queue & make it empty
 def count_longest(q):
     len = 0
-    # FIXME
+    temp_len = 0
+    if q.size() == 0:
+        return 0
+    elif q.size() == 1:
+        return 1
+    else:
+        temp1 = q.deq()
+        temp_len += 1
+    while q.size() > 0:
+        temp2 = q.deq()
+        if temp2 == temp1:
+            temp_len += 1
+        else:
+            temp1 = temp2
+            if temp_len > len: 
+                len = temp_len
+            if q.size() > 0: temp_len = 1
+    if temp_len > len: 
+                len = temp_len
     return len
 
 def main():
     print("out 2:", count_longest(Queue([l for l in "hello"])))
     print("out 5:", count_longest(Queue([l for l in "m" * 5])))
     print("out 3:", count_longest(Queue([l for l in "heee" ])))
+
 
 
 # Don't run main on import
